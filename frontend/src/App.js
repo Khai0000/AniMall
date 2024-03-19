bf445ed6be4e9df65b60a63fcec3f69947899c0b
Khai0000 <ginkhai88@gmail.com>
2024-03-19 21:46:20 +0800

    Added a home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import Pet from "./pages/Pet";
 import Community from "./pages/Community";
 import Product from "./pages/Product";
 import Services from "./pages/Services";
+import Home from "./pages/Home";
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>}/>
         <Route path="/pet" element={<Pet/>}/>
         <Route path="/community" element={<Community/>}/>
         <Route path="/product" element={<Product/>}/>
